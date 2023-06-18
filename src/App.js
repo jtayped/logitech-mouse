4233bc9fb5ad92fad99a591e8b481eb411de52c4
jtayped <87146558+jtayped@users.noreply.github.com>
2023-06-18 11:25:11 +0200

    padding removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Hero, Header, Unleash } from "./containers";
 
 function App() {
   return (
-    <div className="bg-background text-text font-primary pb-[1000px] overflow-x-hidden">
+    <div className="bg-background text-text font-primary overflow-x-hidden">
       <header className="flex flex-col">
         <Header />
         <Hero />
